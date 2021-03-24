3e6334848d90162e09b8dcf4145f935c79798411
Jon <jonathan.p.lamar@gmail.com>
2021-03-24 15:20:28 -0600

    Wrote some basic unit tests.  Still do not really understand flatspec (using it like junit)

--- backend/test/models/actions/UpdateGameStateTest.scala
+++ backend/test/models/actions/UpdateGameStateTest.scala
@@ -1,14 +1,68 @@
 package models.actions
 
-import models.rules.ValidMoves._
+import models.actions.UpdateGameState._
+import models.utils.DataTypes._
+import scala.util.Random.nextInt
 import test.framework.UnitSpec
 
 class UpdateGameStateTest extends UnitSpec {
 
-  /** King moves => no castle. */
+  behavior of "En passant update"
 
-  /** King moves then moves back => no castle. */
+  it should "populate the target after a double pawn push" in {
+    val gameState = GameState("8/pppppppp/8/8/8/8/PPPPPPPP/8 w - - 0 1")
+    val whitePawnPos = Position(6, nextInt(8))
+    val blackPawnPos = Position(1, nextInt(8))
+    val updatedGameState1 = updateGameState(gameState, whitePawnPos, whitePawnPos + Position(-2, 0))
+    val updatedGameState2 =
+      updateGameState(updatedGameState1, blackPawnPos, blackPawnPos + Position(2, 0))
 
-  /** Rook moves => no castle. */
+    updatedGameState1.enPassantTarget should equal(whitePawnPos + Position(-1, 0))
+    updatedGameState2.enPassantTarget should equal(blackPawnPos + Position(1, 0))
+  }
+
+  it should "remove the previous double-pushed pawn" in {
+    val gameState = GameState("8/3p4/8/2P5/8/8/8/8 b - - 0 1")
+    val updatedGameState1 = updateGameState(gameState, Position(1, 3), Position(3, 3))
+    val updatedGameState2 = updateGameState(updatedGameState1, Position(3, 2), Position(2, 3))
+
+    updatedGameState2.squares(3)(3) should be(Blank)
+  }
+
+  behavior of "Castling"
+
+  it should "be disabled after a king move" in {
+    val gameState = GameState("4k3/8/8/8/8/8/8/4K3 b KQkq - 0 1")
+    val updatedGameState1 = updateGameState(gameState, Position(0, 4), Position(1, 4))
+    val updatedGameState2 = updateGameState(updatedGameState1, Position(7, 4), Position(7, 5))
+
+    updatedGameState1.castleStatus should equal(CastleStatus(false, false, true, true))
+    updatedGameState2.castleStatus should equal(CastleStatus(false, false, false, false))
+  }
+
+  it should "be disabled even if king moves back to original position" in {
+    val gameState = GameState("4k3/8/8/8/8/8/2P5/8 b KQkq - 0 1")
+    val updatedGameState1 = updateGameState(gameState, Position(0, 4), Position(1, 4))
+    val updatedGameState2 = updateGameState(updatedGameState1, Position(6, 2), Position(5, 2))
+    val updatedGameState3 = updateGameState(updatedGameState2, Position(1, 4), Position(0, 4))
+
+    updatedGameState3.castleStatus should equal(CastleStatus(false, false, true, true))
+  }
+
+  it should "be disabled for rook after rook moves" in {
+    val gameState = GameState("8/8/8/8/8/8/8/4K2R w KQkq - 0 1")
+    val updatedGameState1 = updateGameState(gameState, Position(7, 7), Position(6, 7))
+
+    updatedGameState1.castleStatus should equal(CastleStatus(true, true, false, true))
+  }
+
+  it should "be disabled for rook even if rook moves back to original position" in {
+    val gameState = GameState("8/p7/8/8/8/8/8/4K2R w KQkq - 0 1")
+    val updatedGameState1 = updateGameState(gameState, Position(7, 7), Position(6, 7))
+    val updatedGameState2 = updateGameState(updatedGameState1, Position(1, 0), Position(2, 0))
+    val updatedGameState3 = updateGameState(updatedGameState2, Position(6, 7), Position(7, 7))
+
+    updatedGameState3.castleStatus should equal(CastleStatus(true, true, false, true))
+  }
 
 }
--- backend/test/models/rules/ValidMovesTest.scala
+++ backend/test/models/rules/ValidMovesTest.scala
@@ -1,20 +1,84 @@
 package models.rules
 
 import models.rules.ValidMoves._
+import models.utils.DataTypes._
 import test.framework.UnitSpec
 
 class ValidMovesTest extends UnitSpec {
 
-  /** En passant target present in pawn move if in correct position */
+  // TODO: All of the asserts for restriction of legal moves should also have a
+  // corresponding assert about all possible moves.  That way we know the check
+  // logic is really limiting moves.
 
-  /** En passant target not present in pawn move if not in correct position */
+  behavior of "En passant target"
 
-  /** Check disables other moves. */
+  it should "be present in pawn move at capture position" in {
+    val gameState = GameState("8/8/8/1Pp5/8/8/8/4K3 w - c6 0 1")
 
-  /** Cannot put self in check. */
+    allPossibleMoves(gameState, Position(3, 1)) should contain theSameElementsAs List(
+      Position(2, 2),
+      Position(2, 1)
+    )
+    getLegalMoves(gameState, Position(3, 1)) should contain(Position(2, 2))
+  }
+
+  it should "not be present in pawn moves if not in capture position" in {
+    val gameState = GameState("8/8/8/2p5/8/8/3P4/4K3 w - c6 0 1")
+
+    allPossibleMoves(gameState, Position(6, 3)) shouldNot contain(Position(2, 2))
+  }
+
+  behavior of "Check"
+
+  it should "disable other moves" in {
+    val gameState = GameState("8/8/8/8/8/8/P7/4K2r w - - 0 1")
+
+    allPossibleMoves(gameState, Position(6, 0)) should contain theSameElementsAs List(
+      Position(5, 0),
+      Position(4, 0)
+    )
+    getLegalMoves(gameState, Position(6, 0)) shouldBe empty
+  }
+
+  it should "not be possible for player to put king in check" in {
+    val gameState = GameState("8/8/8/8/8/8/6r1/3K4 w - - 0 1")
+
+    allPossibleMoves(gameState, Position(7, 3)).filter(p =>
+      p.row == 6
+    ) should contain theSameElementsAs List(
+      Position(6, 2),
+      Position(6, 3),
+      Position(6, 4)
+    )
+    getLegalMoves(gameState, Position(7, 3)).filter(p => p.row == 6) shouldBe empty
+  }
+
+  it should "not be possible for player to expose king to attack" in {
+    val gameState = GameState("8/8/8/8/6b1/8/4P3/3K4 w - - 0 1")
+
+    allPossibleMoves(gameState, Position(6, 4)) should contain theSameElementsAs List(
+      Position(5, 4),
+      Position(4, 4)
+    )
+    getLegalMoves(gameState, Position(6, 4)) shouldBe empty
+  }
 
   /** Cannot en passant if it puts self in check. */
+  it should "not be possible for player to en passant if it puts self in check" in {
+    val gameState = GameState("8/8/8/KPp4r/8/8/8/8 w - c6 0 1")
+
+    allPossibleMoves(gameState, Position(3, 1)) should contain theSameElementsAs List(
+      Position(2, 1),
+      Position(2, 2)
+    )
+    getLegalMoves(gameState, Position(3, 1)) shouldNot contain(Position(2, 2))
+  }
+
+  behavior of "Valid moves generation"
 
   /** E2E valid moves count test (comparing to stockfish) */
+  it should "generate the same move counts as stockfish" in {
+    // TODO
+  }
 
 }
