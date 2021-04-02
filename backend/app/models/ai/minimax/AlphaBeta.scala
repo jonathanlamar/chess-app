5776cf75136f574fd59b7f47a89484b6301fe819
Jon <jonathan.p.lamar@gmail.com>
2021-04-01 21:42:10 -0600

    Basic alpha beta up.

--- backend/app/models/ai/minimax/AlphaBeta.scala
+++ backend/app/models/ai/minimax/AlphaBeta.scala
@@ -6,7 +6,7 @@ import models.rules.Check.isCurrentPlayerInCheck
 import models.rules.ValidMoves._
 import models.utils.DataTypes._
 import scala.collection.parallel.ParSeq
-import scala.math.max
+import scala.math.{min, max}
 
 object AlphaBeta {
   val negativeInfinity = Int.MinValue
@@ -14,41 +14,83 @@ object AlphaBeta {
 
   /** Minmax algorithm with alpha/beta pruning. See
     * https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
+    * This algorithm is inherently serial, since the search of nodes depends on
+    * knowledge of previous searched nodes.
     *
     * @param gameState - current state of play
     * @param depth - max depth of tree to search, where 0 means score current game
     * @param alpha
     * @param beta
+    * @param maximizingPlayer
     * @return tuple containing best move and its advantage
     */
   def search(
       gameState: GameState,
       depth: Int,
       alpha: Int = negativeInfinity,
-      beta: Int = positiveInfinity
+      beta: Int = positiveInfinity,
+      maximizingPlayer: Boolean = true
   ): (MoveLike, Int) = {
-    if (depth == 0) return (NoMove, score(gameState))
+    if (depth == 0) {
+      val thisScore = score(gameState)
+
+      if (maximizingPlayer) return (NoMove, thisScore)
+      else return (NoMove, -thisScore)
+    }
 
     val moves = getAllLegalMoves(gameState)
 
     if (moves.isEmpty) {
       if (isCurrentPlayerInCheck(gameState)) (NoMove, negativeInfinity)
       else (NoMove, 0)
-    } else {
-      ParSeq
-        .fromSpecific(moves)
-        .map(move =>
-          move match {
-            case m: Move => {
-              val updatedGameState = updateGameState(gameState, m)
-              val evaluation = -1 * search(updatedGameState, depth - 1, -beta, -alpha)._2
+    } else if (maximizingPlayer) {
+      var bestScore = negativeInfinity
+      var bestMove: MoveLike = NoMove
+      var newAlpha = alpha
 
-              (m, evaluation)
-            }
-            case NoMove => throw new Exception("getAllLegalMoves returned nonsense")
-          }
-        )
-        .reduce((x, y) => if (x._2 > y._2) x else y)
+      for (move <- moves) {
+
+        val updatedGameState = updateGameState(gameState, move)
+        val (_, newScore) = search(updatedGameState, depth - 1, newAlpha, beta, false)
+
+        if (newScore > bestScore) {
+          bestScore = newScore
+          bestMove = move
+        }
+
+        newAlpha = max(newAlpha, bestScore)
+
+        if (newAlpha >= beta) return (bestMove, bestScore)
+      }
+
+      (bestMove, bestScore)
+    } else {
+      var bestScore = positiveInfinity
+      var bestMove: MoveLike = NoMove
+      var newBeta = beta
+
+      for (move <- moves) {
+
+        val updatedGameState = updateGameState(gameState, move)
+        val (_, newScore) = search(updatedGameState, depth - 1, alpha, newBeta, true)
+
+        if (newScore < bestScore) {
+          bestScore = newScore
+          bestMove = move
+        }
+
+        newBeta = min(newBeta, bestScore)
+
+        if (newBeta <= alpha) return (bestMove, -bestScore)
+      }
+
+      (bestMove, -bestScore)
     }
+  }
+
+  def makeMove(gameState: GameState): GameState = {
+    val move = search(gameState, 4)._1
+
+    updateGameState(gameState, move)
   }
 }
